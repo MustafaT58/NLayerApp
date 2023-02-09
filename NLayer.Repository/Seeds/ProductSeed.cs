5e895f962c80d671a77181ed9ac2350424b2ac9f
null <null@gmail.com>
2023-02-09 20:15:18 +0300

    Update

---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { 
+                    Id = 1, 
+                    CategoryId=1,
+                    Price=100,
+                    Stock=20,
+                    CreateDate=DateTime.Now,
+                    }
+                );
+        }
+    }
+}
